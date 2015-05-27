25d80ac2d39a8165c7f8caaec21266275d583f36
Jeroen Domburg <jeroen@spritesmods.com>
2015-05-27 18:25:01 +0800

    OMGWTF IT PLAYS MP3S!!!!!!!!!11

--- mp3/user/user_main.c
+++ mp3/user/user_main.c
@@ -26,47 +26,32 @@ layer3: 744 bytes rodata
 */
 
 
-//#define server_ip "192.168.40.115"
-#define server_ip "192.168.1.4"
+#define server_ip "192.168.40.117"
+//#define server_ip "192.168.1.4"
 //#define server_ip "192.168.4.100"
 #define server_port 1234
 
-#define READER_THREAD
-//#define UART_AUDIO
-//#define FROMFLASH
-#define SPI_BUFFER
+#define UART_AUDIO
 
-#define READERBUFSIZE 5100
-#define READCHUNKSIZE 256
-
-#ifdef FROMFLASH
-char ICACHE_RODATA_ATTR mp3data[]={
-#include "mp3.dat"
-};
-#endif
 
 struct madPrivateData {
 	int fd;
 	xSemaphoreHandle muxBufferBusy;
 	xSemaphoreHandle semNeedRead;
-#ifdef READER_THREAD
-	char buff[READERBUFSIZE];
-	int buffPos;
-#endif
-#ifdef FROMFLASH
-	char *fpos;
-#endif
+	int fifoLen;
+	int fifoRaddr;
+	int fifoWaddr;
 };
 
 #ifdef UART_AUDIO
 #define printf(a, ...) while(0)
 #endif
 
-#ifdef SPI_BUFFER
+#define SPIRAMSIZE (128*1024)
+#define SPIREADSIZE 64 		//in bytes, needs to be multiple of 4
 
 #define SPI 			0
 #define HSPI			1
-
 
 void spiRamInit() {
 	 //hspi overlap to spi, two spi masters on cspi
@@ -87,11 +72,19 @@ void spiRamInit() {
 	//IT WORK AS HSPI CS2 AFTER OVERLAP(THERE IS NO PIN OUT FOR NATIVE HSPI CS1/2)
 	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO0_U, FUNC_SPICS2);
 
+/*
 	WRITE_PERI_REG(SPI_CLOCK(HSPI), 
 					(((7)&SPI_CLKDIV_PRE)<<SPI_CLKDIV_PRE_S)|
 					(((4)&SPI_CLKCNT_N)<<SPI_CLKCNT_N_S)|
 					(((4)&SPI_CLKCNT_H)<<SPI_CLKCNT_H_S)|
 					(((2)&SPI_CLKCNT_L)<<SPI_CLKCNT_L_S));
+*/
+	WRITE_PERI_REG(SPI_CLOCK(HSPI), 
+					(((1)&SPI_CLKDIV_PRE)<<SPI_CLKDIV_PRE_S)|
+					(((2)&SPI_CLKCNT_N)<<SPI_CLKCNT_N_S)|
+					(((2)&SPI_CLKCNT_H)<<SPI_CLKCNT_H_S)|
+					(((1)&SPI_CLKCNT_L)<<SPI_CLKCNT_L_S));
+
 }
 
 #define SPI_W(i, j)                   (REG_SPI_BASE(i) + 0x40 + ((j)*4))
@@ -106,6 +99,7 @@ void spiRamRead(int addr, char *buff, int len) {
 	WRITE_PERI_REG(SPI_USER1(HSPI), ((0&SPI_USR_MOSI_BITLEN)<<SPI_USR_MOSI_BITLEN_S)| //no data out
 			((((8*len)-1)&SPI_USR_MISO_BITLEN)<<SPI_USR_MISO_BITLEN_S)| //len bits of data in
 			((23&SPI_USR_ADDR_BITLEN)<<SPI_USR_ADDR_BITLEN_S)); //address is 24 bits A0-A23
+	WRITE_PERI_REG(SPI_ADDR(HSPI), addr<<8); //write address
 	WRITE_PERI_REG(SPI_USER2(HSPI), (((7&SPI_USR_COMMAND_BITLEN)<<SPI_USR_COMMAND_BITLEN_S) | 0x03));
 	SET_PERI_REG_MASK(SPI_CMD(HSPI), SPI_USR);
 	while(READ_PERI_REG(SPI_CMD(HSPI))&SPI_USR) ;
@@ -132,7 +126,36 @@ void spiRamWrite(int addr, char *buff, int len) {
 	while(READ_PERI_REG(SPI_CMD(HSPI))&SPI_USR) ;
 }
 
-#endif
+
+void spiRamTest() {
+	int x;
+	int err=0;
+	char a[64];
+	char b[64];
+	char aa, bb;
+	for (x=0; x<64; x++) {
+		a[x]=x^(x<<2);
+		b[x]=x;
+	}
+	spiRamWrite(0x0, a, 64);
+	spiRamWrite(0x100, b, 64);
+
+	spiRamRead(0x0, a, 64);
+	spiRamRead(0x100, b, 64);
+	for (x=0; x<64; x++) {
+		aa=x^(x<<2);
+		bb=x;
+		if (aa!=a[x]) {
+			err=1;
+			printf("aa: 0x%x != 0x%x\n", aa, a[x]);
+		}
+		if (bb!=b[x]) {
+			err=1;
+			printf("bb: 0x%x != 0x%x\n", bb, b[x]);
+		}
+	}
+	while(err);
+}
 
 
 struct madPrivateData madParms;
@@ -159,7 +182,7 @@ void set_dac_sample_rate(int rate) {
 }
 
 //The mp3 read buffer. 2106 bytes should be enough for up to 48KHz mp3s according to the sox sources. Used by libmad.
-#define READBUFSZ 2106
+#define READBUFSZ (2106+64)
 static char readBuf[READBUFSZ]; 
 
 void ICACHE_FLASH_ATTR memcpyAligned(char *dst, char *src, int len) {
@@ -180,50 +203,42 @@ void ICACHE_FLASH_ATTR memcpyAligned(char *dst, char *src, int len) {
 static enum  mad_flow ICACHE_FLASH_ATTR input(void *data, struct mad_stream *stream) {
 	int n, i;
 	int rem, canRead;
+	char rbuf[SPIREADSIZE];
 	struct madPrivateData *p = (struct madPrivateData*)data;
 	//Shift remaining contents of buf to the front
 	rem=stream->bufend-stream->next_frame;
 	memmove(readBuf, stream->next_frame, rem);
 
-#ifdef READER_THREAD
 	//Wait until there is enough data in the buffer. This only happens when the data feed rate is too low, and shouldn't normally be needed!
 	do {
 		xSemaphoreTake(p->muxBufferBusy, portMAX_DELAY);
-		canRead=((p->buffPos)>=(sizeof(readBuf)-rem));
+		canRead=((p->fifoLen)>=(sizeof(readBuf)-rem));
 		xSemaphoreGive(p->muxBufferBusy);
 		if (!canRead) {
-			printf("Buf uflow %d < %d \n", (p->buffPos), (sizeof(readBuf)-rem));
+			printf("Buf uflow %d < %d \n", (p->fifoLen), (sizeof(readBuf)-rem));
 			vTaskDelay(100/portTICK_RATE_MS);
 		}
 	} while (!canRead);
 
+	while (rem<=(READBUFSZ-64)) {
+		//Grab 64 bytes of data from the SPI RAM fifo
+		xSemaphoreTake(p->muxBufferBusy, portMAX_DELAY);
+		spiRamRead(p->fifoRaddr, rbuf, SPIREADSIZE);
+		p->fifoRaddr+=SPIREADSIZE;
+		if (p->fifoRaddr>=SPIRAMSIZE) p->fifoRaddr-=SPIRAMSIZE;
+		p->fifoLen-=SPIREADSIZE;
+		xSemaphoreGive(p->muxBufferBusy);
 
-	//Read in bytes from buffer
-	xSemaphoreTake(p->muxBufferBusy, portMAX_DELAY);
-	n=sizeof(readBuf)-rem; //amount of bytes to re-fill the buffer with
-	memcpy(&readBuf[rem], p->buff, n);
-	//Shift remaining contents of readBuff to the front
-	memmove(p->buff, &p->buff[n], sizeof(p->buff)-n);
-	p->buffPos-=n;
-	xSemaphoreGive(p->muxBufferBusy);
+		//Move into place
+		memcpyAligned(&readBuf[rem], rbuf, SPIREADSIZE);
+		rem+=sizeof(rbuf);
+	}
 
 	//Let reader thread read more data.
 	xSemaphoreGive(p->semNeedRead);
-#elif defined(FROMFLASH)
-	//Copy from flash
-	n=sizeof(readBuf)-i;
-	printf("Copy %p, %d bytes\n", p->fpos, n);
-	memcpyAligned(&readBuf[i], p->fpos, n);
-	printf("Flashbuf: %02x %02x %02x %02x %02x\n", readBuf[i], readBuf[i+1], readBuf[i+2], readBuf[i+3], readBuf[i+4]);
-	p->fpos+=(sizeof(readBuf)-i);
-#else
-	i=rem;
-	while (i<sizeof(readBuf)) {
-		n=read(p->fd, &readBuf[i], sizeof(readBuf)-i);
-		i+=n;
-	}
-#endif
-	mad_stream_buffer(stream, readBuf, sizeof(readBuf));
+
+	//Okay, decode the buffer.
+	mad_stream_buffer(stream, readBuf, rem);
 	return MAD_FLOW_CONTINUE;
 }
 
@@ -271,9 +286,6 @@ int openConn() {
 void ICACHE_FLASH_ATTR tskmad(void *pvParameters){
 	struct mad_decoder decoder;
 	struct madPrivateData *p=&madParms; //pvParameters;
-#if !defined(READER_THREAD) && !defined(FROMFLASH)
-	p->fd=openConn();
-#endif
 //	printf("MAD: Decoder init.\n");
 	mad_decoder_init(&decoder, p, input, 0, 0 , output, error, 0);
 	mad_decoder_run(&decoder, MAD_DECODER_MODE_SYNC);
@@ -281,42 +293,50 @@ void ICACHE_FLASH_ATTR tskmad(void *pvParameters){
 //	printf("MAD: Decode done.\n");
 }
 
-#ifdef READER_THREAD
 void ICACHE_FLASH_ATTR tskreader(void *pvParameters) {
 	struct madPrivateData *p=&madParms;//pvParameters;
 	fd_set fdsRead;
-	int n;
+	int madRunning=0;
+	char wbuf[SPIREADSIZE];
+	int n, l, inBuf;
 	p->fd=-1;
 	while(1) {
 		if (p->fd==-1) p->fd=openConn();
-		printf("Resuming read, can read %d bytes...\n", sizeof(p->buff)-p->buffPos);
-		while (p->buffPos!=sizeof(p->buff)) {
-			//Wait for more data
-			FD_ZERO(&fdsRead);
-			FD_SET(p->fd, &fdsRead);
-			lwip_select(p->fd+1, &fdsRead, NULL, NULL, NULL);
-			//Take buffer mux and read data.
+		printf("Reading into SPI buffer...\n");
+		do {
+			//Grab amount of buffer full-ness
 			xSemaphoreTake(p->muxBufferBusy, portMAX_DELAY);
-
-			n=READCHUNKSIZE;
-			if (n>sizeof(p->buff)-p->buffPos) n=sizeof(p->buff)-p->buffPos;
-			n=read(p->fd, &p->buff[p->buffPos], n);
-			if (n<=0) {
-				close(p->fd);
-				p->fd=-1;
-				break;
-			}
-			p->buffPos+=n;
+			inBuf=p->fifoLen;
 			xSemaphoreGive(p->muxBufferBusy);
-			printf("Sock: %d bytes.\n", n);
-			if (n==0) break; //ToDo: handle eof better
+			if (inBuf<(SPIRAMSIZE-SPIREADSIZE)) {
+				printf("Fifo fill: %d\n", inBuf);
+				//We can add some data. Read data from fd into buffer. Make sure we read 64 bytes.
+				l=0;
+				while (l!=SPIREADSIZE) {
+					n=read(p->fd, &wbuf[l], SPIREADSIZE-l);
+					l+=n;
+					if (n==0) break;
+				}
+				//Write data into SPI fifo. We need to 
+				xSemaphoreTake(p->muxBufferBusy, portMAX_DELAY);
+				spiRamWrite(p->fifoWaddr, wbuf, SPIREADSIZE);
+				p->fifoWaddr+=SPIREADSIZE;
+				if (p->fifoWaddr>=SPIRAMSIZE) p->fifoWaddr-=SPIRAMSIZE;
+				p->fifoLen+=SPIREADSIZE;
+				xSemaphoreGive(p->muxBufferBusy);
+				if (n==0) break; //ToDo: Handle EOF better
+			}
+		} while (inBuf<(SPIRAMSIZE-64));
+		if (!madRunning) {
+			//tskMad seems to need at least 2450 bytes of RAM.
+			if (xTaskCreate(tskmad, "tskmad", 2450, NULL, 2, NULL)!=pdPASS) printf("ERROR! Couldn't create MAD task!\n");
+			madRunning=1;
 		}
 		printf("Read done.\n");
 		//Try to take the semaphore. This will wait until the mad task signals it needs more data.
 		xSemaphoreTake(p->semNeedRead, portMAX_DELAY);
 	}
 }
-#endif
 
 void ICACHE_FLASH_ATTR tskconnect(void *pvParameters) {
 	vTaskDelay(3000/portTICK_RATE_MS);
@@ -330,66 +350,42 @@ void ICACHE_FLASH_ATTR tskconnect(void *pvParameters) {
 
 	struct station_config *config=malloc(sizeof(struct station_config));
 	memset(config, 0x00, sizeof(struct station_config));
-//	sprintf(config->ssid, "wifi-2");
-//	sprintf(config->password, "thesumof6+6=12");
-	sprintf(config->ssid, "Sprite");
-	sprintf(config->password, "pannenkoek");
+	sprintf(config->ssid, "wifi-2");
+	sprintf(config->password, "thesumof6+6=12");
+//	sprintf(config->ssid, "Sprite");
+//	sprintf(config->password, "pannenkoek");
 	wifi_station_set_config(config);
 	wifi_station_connect();
 	free(config);
 //	printf("Connection thread done.\n");
 
-	//tskMad seems to need at least 2450 bytes of RAM.
-	if (xTaskCreate(tskmad, "tskmad", 2450, NULL, 12, NULL)!=pdPASS) printf("ERROR! Couldn't create MAD task!\n");
-#ifdef READER_THREAD
-	if (xTaskCreate(tskreader, "tskreader", 230, NULL, 11, NULL)!=pdPASS) printf("ERROR! Couldn't create reader task!\n");
-#endif
+	if (xTaskCreate(tskreader, "tskreader", 230, NULL, 3, NULL)!=pdPASS) printf("ERROR! Couldn't create reader task!\n");
 	vTaskDelete(NULL);
 }
 
-void ICACHE_FLASH_ATTR tskspitst(void *pvParameters) {
-	int a[4];
-#ifdef SPI_BUFFER
-	spiRamInit();
-	printf("SPI ram INITED\n");
-	while(1) {
-		a[0]=0xdeadbeef;
-		a[1]=0xcafebabe;
-		a[2]=0x12345678;
-		a[3]=0xaa55aa55;
-		printf("Before: %x %x %x %x\n", a[0], a[1], a[2], a[3]);
-//		spiRamWrite(0x10, (char*)a, 8);
-//		spiRamWrite(0x18, (char*)&a[2], 8);
-		spiRamWrite(0, (char*)a, 16);
-		spiRamRead(0, (char*)a, 16);
-		printf("After: %x %x %x %x\n", a[0], a[1], a[2], a[3]);
-		vTaskDelay(500/portTICK_RATE_MS);
-	}
-#endif
-
-
-}
+extern void os_update_cpu_frequency(int mhz);
 
 
 void ICACHE_FLASH_ATTR
 user_init(void)
 {
+	SET_PERI_REG_MASK(0x3ff00014, BIT(0));
+	os_update_cpu_frequency(160);
+	
 #ifdef UART_AUDIO
 	UART_SetBaudrate(0, 481000);
 #else
 	UART_SetBaudrate(0, 115200);
 #endif
+	spiRamInit();
+	spiRamTest();
 
-#ifdef FROMFLASH
-	madParms.fpos=mp3data;
-#endif
+	madParms.fifoLen=0;
+	madParms.fifoRaddr=0;
+	madParms.fifoWaddr=0;
 	madParms.muxBufferBusy=xSemaphoreCreateMutex();
 	vSemaphoreCreateBinary(madParms.semNeedRead);
 //	printf("Starting tasks...\n");
 	xTaskCreate(tskconnect, "tskconnect", 200, NULL, 3, NULL);
-
-
-	if (xTaskCreate(tskspitst, "tskspitst", 230, NULL, 11, NULL)!=pdPASS) printf("ERROR! Couldn't create reader task!\n");
-
 }
 
