670628c01e5575a3357749c380c1f9e4fe0f44df
Jeroen Domburg <jeroen@spritesmods.com>
2015-06-03 09:49:21 +0800

    Forgot fifo header file

---
+++ mp3/include/spiram_fifo.h
@@ -0,0 +1,10 @@
+#ifndef _SPIRAM_FIFO_H_
+#define _SPIRAM_FIFO_H_
+
+int ICACHE_FLASH_ATTR spiRamFifoInit();
+void ICACHE_FLASH_ATTR spiRamFifoRead(char *buff, int len);
+void ICACHE_FLASH_ATTR spiRamFifoWrite(char *buff, int len);
+int ICACHE_FLASH_ATTR spiRamFifoFill();
+int ICACHE_FLASH_ATTR spiRamFifoFree();
+
+#endif
